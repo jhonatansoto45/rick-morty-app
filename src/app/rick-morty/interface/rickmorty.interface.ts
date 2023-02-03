a5e5cfee1fca281e335fd214fa02aab343e2001d
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-03 17:52:04 -0500

    Tabla de premios, aun desarrollo

--- src/app/rick-morty/interface/rickmorty.interface.ts
+++ src/app/rick-morty/interface/rickmorty.interface.ts
@@ -2,3 +2,9 @@ export interface ImagenPhoto {
   nombre: string;
   alt: string;
 }
+
+export interface TablaPremiacion{
+  anio: number;
+  premio: string;
+  categoria: string;
+}
--- src/app/rick-morty/pages/home/home.component.ts
+++ src/app/rick-morty/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ImagenPhoto } from '../../interface/rickmorty.interface';
+import {
+  ImagenPhoto,
+  TablaPremiacion,
+} from '../../interface/rickmorty.interface';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +14,57 @@ export class HomeComponent implements OnInit {
   listPartnes: ImagenPhoto[] = [
     {
       nombre: 'hbo1',
-      alt: 'HBO'
+      alt: 'HBO',
     },
     {
       nombre: 'cartoon',
-      alt: 'Cartoon Network'
+      alt: 'Cartoon Network',
+    },
+  ];
+
+  listPremios: TablaPremiacion[] = [
+    {
+      anio: 2015,
+      premio: 'BTVA Voice Acting Awards',
+      categoria:
+        'Best Male Lead Vocal Performance in a Television Series — Comedy/Musical',
+    },
+    {
+      anio: 2015,
+      premio: 'IGN Awards',
+      categoria: 'Best Animated Series',
+    },
+    {
+      anio: 2015,
+      premio: "IGN People's Choice Award",
+      categoria: 'Best Animated Series',
+    },
+    {
+      anio: 2017,
+      premio: 'IGN Awards',
+      categoria: 'Best Animated Series',
+    },
+    {
+      anio: 2017,
+      premio: 'IGN Awards',
+      categoria: 'Best Comedic TV Performance',
+    },
+    {
+      anio: 2018,
+      premio: "Critics' Choice Television Awards",
+      categoria: 'Best Animated Series',
+    },
+    {
+      anio: 2018,
+      premio: 'Annie Awards',
+      categoria:
+        'Best General Audience Animated Television/Broadcast Production',
+    },
+    {
+      anio: 2018,
+      premio: 'Annie Awards',
+      categoria:
+        'Outstanding Achievement for Writing in an Animated Television/Broadcast Production',
     },
   ];
 
